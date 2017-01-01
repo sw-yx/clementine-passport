b77a27a165d8a309ba8fa2598e8618a3e7bd2e22
swyx <shawnthe1@gmail.com>
2017-01-01 11:09:38 +0000

    initial clementine build

---
+++ app/controllers/clickController.client.js
@@ -0,0 +1,52 @@
+'use strict';
+
+(function () {
+   var addButton = document.querySelector('.btn-add');
+   var deleteButton = document.querySelector('.btn-delete');
+   var clickNbr = document.querySelector('#click-nbr');
+   var apiUrl = 'https://base-clementine-swyx.c9users.io/api/clicks';
+
+   function ready (fn) {
+      if (typeof fn !== 'function') {
+         return;
+      }
+
+      if (document.readyState === 'complete') {
+         return fn();
+      }
+
+      document.addEventListener('DOMContentLoaded', fn, false);
+   }
+   function ajaxRequest (method, url, callback) {
+      var xmlhttp = new XMLHttpRequest();
+    
+      xmlhttp.onreadystatechange = function () {
+         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            callback(xmlhttp.response);
+         }
+      };
+    
+      xmlhttp.open(method, url, true);
+      xmlhttp.send();
+    }
+   function updateClickCount (data) {
+      var clicksObject = JSON.parse(data);
+      clickNbr.innerHTML = clicksObject.clicks;
+    }
+    
+    ready(ajaxRequest('GET', apiUrl, updateClickCount));
+     addButton.addEventListener('click', function () {
+
+      ajaxRequest('POST', apiUrl, function () {
+         ajaxRequest('GET', apiUrl, updateClickCount)
+      });
+
+   }, false);
+   deleteButton.addEventListener('click', function () {
+
+      ajaxRequest('DELETE', apiUrl, function () {
+         ajaxRequest('GET', apiUrl, updateClickCount);
+      });
+
+   }, false); 
+})();
